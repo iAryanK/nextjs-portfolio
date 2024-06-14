acbe7c1ca303749f7e00be171dfbe5bf581fba22
Aryan <aryaninmail@gmail.com>
2024-06-14 21:18:08 +0530

    updated: images to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+import { hostname } from 'os';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["github.com"]
+        remotePatterns: [
+            {
+                hostname: 'github.com'
+            }
+        ]
     }
 };
 
